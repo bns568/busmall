c81a5a5a1224bc0e8c3f744e9d496430833a96e5
‘Brittany <brittany.stubbs1827@gmail.com>
2018-08-01 20:32:17 -0400

    added bar chart and pic refresh

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ let gifIndex3;
 let Gif = function(name, filePath, clicked) {
     this.name = name;
     this.filePath = filePath;
+    this.shown = 0;
     this.clicked = clicked;
 }
 
@@ -48,6 +49,10 @@ let randomGif1 = function() {
     //retrieve random gif from array
     gifIndex1 = gifArray[randomIndex];
 
+    //add 1 to shown property
+    gifIndex1.shown ++;
+    console.log(gifIndex1.name + " shown: " + gifIndex1.shown)
+
     //assign src of gif tag to the random gif
     elGif1.src = gifIndex1.filePath;
 }
@@ -63,6 +68,10 @@ let randomGif2 = function() {
 
         gifIndex2 = gifArray[randomIndex];
     }
+
+    //add 1 to shown property
+    gifIndex2.shown ++;
+    console.log(gifIndex2.name + " shown: " + gifIndex2.shown)
 
     //assign src of gif tag to the random gif
     elGif2.src = gifIndex2.filePath;
@@ -80,8 +89,13 @@ let randomGif3 = function() {
         gifIndex3 = gifArray[randomIndex];
     }
 
+    //add 1 to shown property
+    gifIndex3.shown ++;
+    console.log(gifIndex3.name + " shown: " + gifIndex3.shown)
+
     //assign src of gif tag to the random gif
     elGif3.src = gifIndex3.filePath;
+    console.log("gif Index 3 = " + gifIndex3)
 }
 
 //creating event handler to track number of times random gif 1 is clicked
@@ -89,6 +103,55 @@ let gifClick1 = function(e) {
     //accessing clicked property on random image and incrementing by 1 each time clicked
     gifIndex1.clicked += 1;
     console.log(gifIndex1.clicked);
+
+    chart = new Chart(ctx, {
+        //setting the type of chart to bar
+        type: 'bar',
+        //giving data property to chart
+        data: {
+            //setting the lables as image names
+            labels: [gif1.name, gif2.name, gif3.name, gif4.name, gif5.name, gif6.name, gif7.name, gif8.name, gif9.name, gif10.name, gif11.name, gif12.name, gif13.name, gif14.name, gif15.name, gif16.name, gif17.name, gif18.name, gif19.name, gif20.name],
+            //passing in array of datasets to populate individual bars, each dataset will create a new bar for our chart
+            datasets: [
+            {
+                label: 'Displayed',
+                backgroundColor: 'rgb(99, 99, 99)',
+                borderColor: 'rgb(99, 99, 99)',
+                //passing in clicked property as data to calculate bars for each label
+                data: [gif1.shown, gif2.shown, gif3.shown, gif4.shown, gif5.shown, gif6.shown, gif7.shown, gif8.shown, gif9.shown, gif10.shown, gif11.shown, gif12.shown, gif13.shown, gif14.shown, gif15.shown, gif16.shown, gif17.shown, gif18.shown, gif19.shown, gif20.shown]
+            },
+            {
+                //setting the label of the key to Clicked and giving bars styles
+                label: 'Clicked',
+                backgroundColor: 'rgb(255, 99, 130)',
+                borderColor: 'rgb(255, 99, 130)',
+                //passing in clicked property as data to calculate bars for each label
+                data: [gif1.clicked, gif2.clicked, gif3.clicked, gif4.clicked, gif5.clicked, gif6.clicked, gif7.clicked, gif8.clicked, gif9.clicked, gif10.clicked, gif11.clicked, gif12.clicked, gif13.clicked, gif14.clicked, gif15.clicked, gif16.clicked, gif17.clicked, gif18.clicked, gif19.clicked, gif20.clicked]
+            }
+        ],
+        options: {
+            scaleShowValues: true,
+            scales: {
+                xAxes: [
+                    {
+                        stacked: false, 
+                        ticks: {
+                            stepSize: 1,
+                            min: 0,
+                            autoSkip: false
+                        }
+                    }
+                ]
+            }
+            
+        }
+        }
+    })
+    console.log("working")
+
+    randomGif1(); 
+    randomGif2();
+    randomGif3(); 
 }
 
 //creating event handler to track number of times random gif 2 is clicked
@@ -96,6 +159,38 @@ let gifClick2 = function(e) {
     //accessing clicked property on random image and incrementing by 1 each time clicked
     gifIndex2.clicked += 1;
     console.log(gifIndex2.clicked);
+
+    // chart = new Chart(ctx, {
+    //     //setting the type of chart to bar
+    //     type: 'bar',
+    //     //giving data property to chart
+    //     data: {
+    //         //setting the lables as image names
+    //         labels: [gif1.name, gif2.name, gif3.name, gif4.name, gif5.name, gif6.name, gif7.name, gif8.name, gif9.name, gif10.name, gif11.name, gif12.name, gif13.name, gif14.name, gif15.name, gif16.name, gif17.name, gif18.name, gif19.name, gif20.name],
+    //         //passing in array of datasets to populate individual bars, each dataset will create a new bar for our chart
+    //         datasets: [
+    //         {
+    //             label: 'Displayed',
+    //             backgroundColor: 'rgb(99, 99, 99)',
+    //             borderColor: 'rgb(99, 99, 99)',
+    //             //passing in clicked property as data to calculate bars for each label
+    //             data: [gif1.shown, gif2.shown, gif3.shown, gif4.shown, gif5.shown, gif6.shown, gif7.shown, gif8.shown, gif9.shown, gif10.shown, gif11.shown, gif12.shown, gif13.shown, gif14.shown, gif15.shown, gif16.shown, gif17.shown, gif18.shown, gif19.shown, gif20.shown]
+    //         },
+    //         {
+    //             //setting the label of the key to Clicked and giving bars styles
+    //             label: 'Clicked',
+    //             backgroundColor: 'rgb(255, 99, 130)',
+    //             borderColor: 'rgb(255, 99, 130)',
+    //             //passing in clicked property as data to calculate bars for each label
+    //             data: [gif1.clicked, gif2.clicked, gif3.clicked, gif4.clicked, gif5.clicked, gif6.clicked, gif7.clicked, gif8.clicked, gif9.clicked, gif10.clicked, gif11.clicked, gif12.clicked, gif13.clicked, gif14.clicked, gif15.clicked, gif16.clicked, gif17.clicked, gif18.clicked, gif19.clicked, gif20.clicked]
+    //         }
+    //     ]
+    //     }
+    // })
+    console.log("working")
+    randomGif1(); 
+    randomGif2();
+    randomGif3(); 
 }
 
 //creating event handler to track number of times random gif 3 is clicked
@@ -103,7 +198,44 @@ let gifClick3 = function(e) {
     //accessing clicked property on random image and incrementing by 1 each time clicked
     gifIndex3.clicked += 1;
     console.log(gifIndex3.clicked);
+
+    // chart = new Chart(ctx, {
+    //     //setting the type of chart to bar
+    //     type: 'bar',
+    //     //giving data property to chart
+    //     data: {
+    //         //setting the lables as image names
+    //         labels: [gif1.name, gif2.name, gif3.name, gif4.name, gif5.name, gif6.name, gif7.name, gif8.name, gif9.name, gif10.name, gif11.name, gif12.name, gif13.name, gif14.name, gif15.name, gif16.name, gif17.name, gif18.name, gif19.name, gif20.name],
+    //         //passing in array of datasets to populate individual bars, each dataset will create a new bar for our chart
+    //         datasets: [
+    //         {
+    //             label: 'Displayed',
+    //             backgroundColor: 'rgb(99, 99, 99)',
+    //             borderColor: 'rgb(99, 99, 99)',
+    //             //passing in clicked property as data to calculate bars for each label
+    //             data: [gif1.shown, gif2.shown, gif3.shown, gif4.shown, gif5.shown, gif6.shown, gif7.shown, gif8.shown, gif9.shown, gif10.shown, gif11.shown, gif12.shown, gif13.shown, gif14.shown, gif15.shown, gif16.shown, gif17.shown, gif18.shown, gif19.shown, gif20.shown]
+    //         },
+    //         {
+    //             //setting the label of the key to Clicked and giving bars styles
+    //             label: 'Clicked',
+    //             backgroundColor: 'rgb(255, 99, 130)',
+    //             borderColor: 'rgb(255, 99, 130)',
+    //             //passing in clicked property as data to calculate bars for each label
+    //             data: [gif1.clicked, gif2.clicked, gif3.clicked, gif4.clicked, gif5.clicked, gif6.clicked, gif7.clicked, gif8.clicked, gif9.clicked, gif10.clicked, gif11.clicked, gif12.clicked, gif13.clicked, gif14.clicked, gif15.clicked, gif16.clicked, gif17.clicked, gif18.clicked, gif19.clicked, gif20.clicked]
+    //         }
+    //     ]
+    //     }
+    // })
+    console.log("working")
+    randomGif1(); 
+    randomGif2();
+    randomGif3(); 
 }
+
+//get canvas element by ID
+let elCanvas = document.getElementById('chart');
+//set canvas element context as 2d
+let ctx = elCanvas.getContext('2d');
 
 //attaching event listener to image tag
 elGif1.addEventListener('click', gifClick1);
